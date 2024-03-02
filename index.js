16bc90aed69624cfafd0eb636c88b9cff81156c7
Vishnu Bagde <vishnu.bagde@pivotroots.com>
2024-03-02 18:56:30 +0530

    initial push

---
+++ index.js
@@ -0,0 +1,137 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require("dotenv").config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function connectToMongoDB() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.message);
+    }
+}
+
+connectToMongoDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/active-customers', async (req, res) => {
+    try {
+        const activeCustomers = await client.db().collection('customers').find({ active: true }).toArray();
+        console.log('Active Customers:', activeCustomers);
+        return res.json(activeCustomers);
+    } catch (error) {
+        console.error('Error fetching active customers:', error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.get('/api/get-transaction', async (req, res) => {
+    try {
+        const accountId = parseInt(req.query.account_id, 10);
+        console.log('Received account_id:', accountId);
+
+        const transaction = await client.db().collection('transactions').findOne({ account_id: accountId });
+        console.log('Transaction:', transaction);
+
+        return res.json(transaction);
+    } catch (error) {
+        console.error('Error fetching transactions:', error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.post('/api/low-amount-accounts', async (req, res) => {
+    try {
+        const accounts = req.body.accounts;
+        console.log(accounts);
+
+        const filteredAccounts = [];
+
+        for (const account of accounts) {
+            const lowAmountAccounts = await client
+                .db()
+                .collection('transactions')
+                .aggregate([
+                    {
+                        $match: {
+                            account_id: account,
+                            "transactions": {
+                                $elemMatch: {
+                                    "amount": { $lt: 5000 }
+                                }
+                            }
+                        }
+                    },
+                    {
+                        $group: {
+                            _id: null,
+                            account_id: { $first: '$account_id' }
+                        }
+                    },
+                    {
+                        $project: {
+                            _id: 0,
+                            account_id: 1
+                        }
+                    }
+                ])
+                .toArray();
+
+            if (lowAmountAccounts.length > 0) {
+                filteredAccounts.push(account);
+            }
+        }
+        return res.json(filteredAccounts);
+    } catch (error) {
+        console.error('Error fetching low amount accounts:', error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.get('/api/distinct-products', async (req, res) => {
+    try {
+        const disntinctProducts = await client.db().collection('accounts').aggregate([
+            {
+                $unwind: '$products',
+            },
+            {
+                $group: {
+                    _id: null,
+                    distinctProducts: { $addToSet: '$products' },
+                },
+            },
+            {
+                $project: {
+                    _id: 0,
+                    distinctProducts: 1,
+                },
+            },
+        ]).toArray();
+
+        return res.json(disntinctProducts);
+    } catch (error) {
+        console.error('Error fetching distinct products:', error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
